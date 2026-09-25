b6f4a2b3cfdedec65db21286ca2f02e3743cb342
kim san <rlatks1217@gmail.com>
2026-09-25 18:13:43 +0900

    chore 프로젝트 생성

---
+++ src/test/java/com/back/ddd_commerce_mission/DddCommerceMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.ddd_commerce_mission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddCommerceMissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
